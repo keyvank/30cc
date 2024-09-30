34eaf718ac933fe4fab1d3025ad008ede7d6a995
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2024-09-30 22:49:33 +0330

    Handle parenthesis

--- parser/expr.c
+++ parser/expr.c
@@ -11,11 +11,29 @@ parser_node *parse_expr(typed_token **tkns_ptr)
     typed_token *tkn = *tkns_ptr;
     parser_node *ret = NULL;
 
-    ret = parse_binary_op(&tkn);
+    /*ret = parse_binary_op(&tkn);
     if (ret)
     {
         *tkns_ptr = tkn;
         return ret;
+    }*/
+    if (tkn->type_id == TKN_L_PAREN)
+    {
+        tkn = tkn->next;
+        parser_node *inner = parse_expr(&tkn);
+        if (inner)
+        {
+            if (tkn->type_id == TKN_R_PAREN)
+            {
+                tkn = tkn->next;
+                *tkns_ptr = tkn;
+                return inner;
+            }
+        }
+        else
+        {
+            return NULL;
+        }
     }
 
     ret = parse_literal(&tkn);
