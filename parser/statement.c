0c3aaf61605327dde515718c2951d30103c99153
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2024-10-01 19:27:45 +0330

    Implement compound-statements

--- parser/statement.c
+++ parser/statement.c
@@ -7,11 +7,72 @@
 #include "var_decl.h"
 #include "expr.h"
 
+void compound_statement_debug(int depth, parser_node *node)
+{
+    node_compound_statement *comp = (node_compound_statement *)node->data;
+    printtabs(depth);
+    printf("CompoundStatement\n");
+    for (int i = 0; i < comp->num_statements; i++)
+    {
+        comp->statements[i]->debug(depth + 1, comp->statements[i]);
+    }
+}
+
+parser_node *parse_compound_statement(typed_token **tkns_ptr)
+{
+    typed_token *tkn = *tkns_ptr;
+
+    if (tkn->type_id == TKN_L_BRACE)
+    {
+        int num_stmts = 0;
+        parser_node **stmts = (parser_node **)malloc(sizeof(parser_node *) * 128);
+        for (int i = 0; i < 128; i++)
+            stmts[i] = NULL;
+        tkn = tkn->next;
+        while (tkn)
+        {
+            if (tkn->type_id == TKN_R_BRACE)
+            {
+                tkn = tkn->next;
+                *tkns_ptr = tkn;
+
+                parser_node *node = (parser_node *)malloc(sizeof(parser_node));
+                node->type = NODE_FUNCTION;
+                node->data = (void *)malloc(sizeof(node_compound_statement));
+                node->debug = compound_statement_debug;
+                node_compound_statement *comp = (node_compound_statement *)node->data;
+                comp->num_statements = num_stmts;
+                comp->statements = stmts;
+
+                return node;
+            }
+            parser_node *stmt = parse_statement(&tkn);
+            if (stmt)
+            {
+                stmts[num_stmts++] = stmt;
+            }
+            else
+            {
+                return NULL;
+            }
+        }
+    }
+
+    return NULL;
+}
+
 parser_node *parse_statement(typed_token **tkns_ptr)
 {
     typed_token *tkn = *tkns_ptr;
     parser_node *ret = NULL;
     ret = parse_var_decl(&tkn);
+    if (ret)
+    {
+        *tkns_ptr = tkn;
+        return ret;
+    }
+
+    ret = parse_compound_statement(&tkn);
     if (ret)
     {
         *tkns_ptr = tkn;
--- parser/statement.h
+++ parser/statement.h
@@ -6,4 +6,10 @@
 
 parser_node *parse_statement(typed_token **tkns_ptr);
 
+typedef struct
+{
+    int num_statements;
+    parser_node **statements;
+} node_compound_statement;
+
 #endif
