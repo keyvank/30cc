076120cfbb72038a9fc10fe7199da03a8301a164
Glyphack <sh.hooshyari@gmail.com>
2024-11-09 15:48:52 +0100

    Use kind to determine the type

--- codegen/codegen.c
+++ codegen/codegen.c
@@ -340,9 +340,9 @@ general_type *new_struct_type(char *struct_name)
 int types_equal(general_type *a, general_type *b, context *ctx)
 {
     // Special case for handling assigning NULL to pointers
-    if (a->size == pointer_type_size)
+    if (a->kind == TYPE_POINTER)
     {
-        if (b->size == primitive_type_size)
+        if (b->kind == TYPE_PRIMITIVE)
         {
             char *type_name = ((primitive_type *)b->data)->type_name;
             if (strcmp(type_name, "TKN_INT") == 0)
