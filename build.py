81571cf878927bef7f902d96b723507e8a61abd1
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2024-12-03 12:30:36 +0330

    feat: Add linking phase to the build script

--- build.py
+++ build.py
@@ -12,14 +12,14 @@ def compile(path):
         ["./a.out", path, "--asm"],
         capture_output=True,
         text=True,
-        check=True,
     )
     if out.returncode != 0:
-        raise Exception()
+        raise Exception(out.stderr)
     else:
         return out.stdout
 
 
+has_error = False
 for f in glob.glob("**/*.c", recursive=True):
     try:
         asm = compile(f)
@@ -30,5 +30,15 @@ for f in glob.glob("**/*.c", recursive=True):
         with io.open(asm_filename, "w") as f:
             f.write(asm)
         subprocess.run(["nasm", "-f", "elf64", asm_filename, "-o", obj_filename])
-    except:
-        print("Error compiling:", f)
+    except Exception as e:
+        has_error = True
+        print("Error compiling:", f, e)
+        
+
+objs = list(glob.glob("target/obj/**/*.o", recursive=True))
+
+if not has_error:
+    subprocess.run(
+        ["ld", "-dynamic-linker", "/lib64/ld-linux-x86-64.so.2", "-lc", "-o", "target/30cc"]
+        + objs
+    )
